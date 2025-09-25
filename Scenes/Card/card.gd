0a99ec33dcd42a52ac7f56e80ec02a2cc4f35bfb
uxabix <kirill.olishkevitch@yandex.ru>
2025-09-25 20:23:22 +0200

    Cards are now dynamicaly updating in editor, card flip

--- Scenes/Card/card.gd
+++ Scenes/Card/card.gd
@@ -1,23 +1,40 @@
+@tool
 extends Node2D
 const Card = preload("res://Scenes/Card/card_defines.gd")
 
-@export var suit: Card.Suit
-@export var rank: Card.Rank
+
+@export var is_face_up: bool = true:
+	set(value):
+		is_face_up = value
+		flip()
+@export var suit: Card.Suit:
+	set(value):
+		suit = value
+		set_textures()
+@export var rank: Card.Rank:
+	set(value):
+		rank = value
+		set_textures()
+		set_text()
 @export var type: Card.Type
 
+func flip():
+	$Front.visible = is_face_up
+	$Back.visible = not is_face_up
 
 func set_textures():
-	for i in $Suits.get_children():
+	for i in $Front/Suits.get_children():
 		i.texture = load("res://Scenes/Card/Textures/Suits/"
 		 + Card.get_suit_name(suit) + "/Suit.png")
-	$Images/Rank.visible = rank in Card.HAS_TEXTURE
-	$Images/Suits.visible = not $Images/Rank.visible
-	if $Images/Rank.visible:
-		$Images/Rank.texture = load("res://Scenes/Card/Textures/Suits/" + Card.get_suit_name(suit) + 
-		"/Ranks/" + Card.get_rank_name(rank) + ".png")
+	$Front/Images/Rank.visible = rank in Card.HAS_TEXTURE
+	$Front/Images/Suits.visible = not $Front/Images/Rank.visible
+	if $Front/Images/Rank.visible:
+		$Front/Images/Rank.texture = load("res://Scenes/Card/Textures/Suits/"
+		 + Card.get_suit_name(suit) + "/Ranks/"
+		 + Card.get_rank_name(rank) + ".png")
 	else:
 		var i = 0
-		for image in $Images/Suits.get_children():
+		for image in $Front/Images/Suits.get_children():
 			image.visible = Card.patterns[rank][i]
 			image.texture = load("res://Scenes/Card/Textures/Suits/"
 			 + Card.get_suit_name(suit) + "/Suit.png")
@@ -25,7 +42,7 @@ func set_textures():
 			
 			
 func set_text():
-	for i in $Ranks/Control.get_children():
+	for i in $Front/Ranks/Control.get_children():
 		var name = Card.get_rank_name(rank)
 		name = str(rank + Card.FIRST_CARD_VALUE) if name == "" else name
 		i.text = name if len(name) < 3 else name[0] 
